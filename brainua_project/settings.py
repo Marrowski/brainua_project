f8e2209b84d0106aa984906d18d3e9ad611abb66
Oleh Yemelianov <emelianov2811@gmail.com>
2025-11-20 22:32:50 +0200

    Set-up settings to deploy

--- brainua_project/settings.py
+++ brainua_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)%#jxgqx@8xe6-y1a^@dpkx%ax^#=rc7c96t&)pys&%)ij)7#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-service.onrender.com']
 
 
 # Application definition
@@ -121,9 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://your-service.onrender.com']

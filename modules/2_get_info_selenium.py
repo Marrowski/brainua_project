8f8f9c8046277038ace65d8ae013b86a7f2183d8
Oleh Yemelianov <emelianov2811@gmail.com>
2025-11-23 00:54:08 +0200

    Added main logic to the script

--- modules/2_get_info_selenium.py
+++ modules/2_get_info_selenium.py
@@ -3,11 +3,14 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
 
 from load_django import *
 from parser_app.models import Phone
+
 
 
 def handle_browser():
@@ -31,24 +34,28 @@ def handle_url(driver, url: str):
 
 
 def go_to_the_page(driver):
+
     try:
-        search_bar = driver.find_element(By.XPATH, "//input[@type='search']")
+        search_bar = driver.find_element(By.XPATH, '//div[@class="header-bottom"]//input[@type="search"]')
         print('Search bar has successfully found.')
-        sleep(1)
+        sleep(3)
+
+        search_bar.click()
 
         phone_name = 'Apple iPhone 15 128GB Black'
         search_bar.send_keys(phone_name)
-        print('Value has been successfully proceeded')
-        sleep(1)
+
+        print('Value has been successfully proceeded.')
+        sleep(3)
 
     except NoSuchElementException:
         print('No search bar located on the page.')
 
     try:
-        button = driver.find_element(By.XPATH, '//input[@class="search-button-first-form"]')
-        print('Button is found')
+        button = driver.find_element(By.XPATH, '//div[@class="header-bottom"]//input[@class="qsr-submit"]')
+        print('Button is found.')
         button.click()
-        sleep(1)
+        sleep(3)
 
     except NoSuchElementException:
         print('No search button found.')
@@ -56,11 +63,16 @@ def go_to_the_page(driver):
 
 def handle_search_results(driver):
     try:
-        first_result = driver.find_element(By.XPATH, '//div[@class="col-lg-3 col-md-4 col-sm-6 col-xs-6 product-wrapper br-pcg-product-wrapper"][1]')
+        first_result = wait(driver, 5).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '(//div[contains(@class,"description-wrapper")]//a)[1]')
+            )
+        )
+
         print('First result of matched query found.')
         first_result.click()
         print('Successful redirect...')
-        sleep(1)
+        sleep(5)
 
     except NoSuchElementException:
         print('Element has not been found.')
@@ -70,37 +82,103 @@ def extract_data_from_page(driver):
     print('*' * 100)
 
     try:
-        content = driver.find_element(By.XPATH, '//div[@class="br-body br-body-product"]')
-
+        specs_container = driver.find_element(By.XPATH, '(//div[@class="br-pr-chr-item"])[1]/parent::div')
     except NoSuchElementException:
-        content = None
-        print('Page content not found')
+        specs_container = None
+        print('Information about phone not found.')
 
-    if content:
+    if specs_container:
         phone_data = {}
 
         try:
-            phone_data['name'] = content.find_element(By.XPATH, '//h1[@class="main-title"]').text
+            phone_data['name'] = specs_container.find_element(By.XPATH, './/span[contains(text(), "Модель")]').find_element(By.XPATH, 'following-sibling::span').get_attribute('textContent').strip()
         except NoSuchElementException:
             phone_data['name'] = None
 
-        if not phone_data['name']:
-            return
-
         try:
-            phone_data['color'] = content.find_element('//span[contains(text(), "Колір")]').find_element(By.XPATH, 'following::a[1]').text.strip()
-
+            phone_data['color'] = specs_container.find_element(By.XPATH,'.//span[contains(text(), "Колір")]').find_element(By.XPATH, 'following::a[1]').get_attribute('textContent').strip()
         except NoSuchElementException:
             phone_data['color'] = None
 
+        try:
+            phone_data['memory'] = specs_container.find_element(By.XPATH, './/span[contains(text(), "Вбудована пам\'ять")]').find_element(By.XPATH, 'following::a[1]').get_attribute('textContent').strip()
+        except NoSuchElementException:
+            phone_data['memory'] = None
+
+        try:
+            phone_data['seller'] = specs_container.find_element(By.XPATH, './/span[contains(text(), "Виробник")]').find_element(By.XPATH,'following-sibling::span[1]').get_attribute('textContent').strip()
+        except NoSuchElementException:
+            phone_data['seller'] = None
+
+        try:
+            img_urls = []
+            img_array = driver.find_elements(By.XPATH, './/img[@class="dots-image"]')
+
+            for img in img_array:
+                src = img.get_attribute('src')
+
+                if src:
+                    img_urls.append(src)
+
+            phone_data['photos'] = img_urls if img_urls else None
+        except NoSuchElementException:
+            phone_data['photos'] = None
+
+        try:
+            phone_data['code'] = driver.find_element(By.XPATH, './/span[@class="br-pr-code-val"]').text.strip()
+        except NoSuchElementException:
+            phone_data['code'] = None
+
+        try:
+            reviews = specs_container.find_element(By.XPATH, './/div[@id="reviews-list"]').find_element(By.XPATH, '//div[@class="br-pt-bc-list"]').find_element(By.XPATH, "following-sibling::p").text.strip()
+            phone_data['reviews'] = len(reviews)
+        except NoSuchElementException:
+            phone_data['reviews'] = None
+
+        try:
+            phone_data['diagonal'] = specs_container.find_element(By.XPATH, './/span[contains(text(), "Діагональ екрану")]').find_element(By.XPATH, 'following::a[1]').get_attribute('textContent').strip()
+        except NoSuchElementException:
+            phone_data['diagonal'] = None
+
+        try:
+            phone_data['resolution'] = specs_container.find_element(By.XPATH, './/span[contains(text(), "Роздільна здатність екрану")]').find_element(By.XPATH, 'following::span[1]').get_attribute('textContent').strip()
+        except NoSuchElementException:
+            phone_data['resolution'] = None
+
+        try:
+            phone_specs = {}
+            container = specs_container.find_elements(By.XPATH, './/h3')
+
+            for data in container:
+
+                row = data.find_elements(By.XPATH, 'following-sibling::div[1]//div[span and span[2]]')
+
+                for j in row:
+                    try:
+                        name_specs = j.find_element(By.XPATH, './/span[1]').text.strip()
+                        value_specs = j.find_element(By.XPATH, './/span[2]').text.strip()
+                    except NoSuchElementException:
+                        continue
+
+                    if name_specs and value_specs:
+                        phone_specs[name_specs] = value_specs
 
 
+            phone_data['specs'] = phone_specs if phone_specs else None
 
+        except NoSuchElementException:
+            phone_data['specs'] = None
 
-
-
-
+        for key, value in phone_data.items():
+            print(f'{key}:{value}')
 
 
 if __name__ == '__main__':
-    pass
+    driver = handle_browser()
+    sleep(1)
+    handle_url(driver, 'https://brain.com.ua/')
+    go_to_the_page(driver)
+    handle_search_results(driver)
+    extract_data_from_page(driver)
+    sleep(10)
+    quit_driver(driver)
